89a518750aef766616d7d00f2611fdbedb59c8be
Anup Tamang <anupkumartamang@gmail.com>
2020-03-17 21:45:21 +0545

    covid initial added

--- 3.covid-19-tracker/src/js/app.js
+++ 3.covid-19-tracker/src/js/app.js
@@ -2,13 +2,65 @@
 import "isomorphic-fetch";
 import '../scss/main.scss';
 
-console.log('hello webpack');
+const allCases = document.querySelector('.all-cases');
+const allDeaths = document.querySelector('.all-deaths');
+const allRecovered = document.querySelector('.all-recovered');
 
-async function getUserAsync(name) {
-    let response = await fetch(`https://api.github.com/users/${name}`);
+const nepalCases = document.querySelector('.nepal-cases');
+const nepalDeaths = document.querySelector('.nepal-deaths');
+const nepalRecovered = document.querySelector('.nepal-recovered');
+
+const deCases = document.querySelector('.de-cases');
+const deDeaths = document.querySelector('.de-deaths');
+const deRecovered = document.querySelector('.de-recovered');
+
+async function getAllCases() {
+    let response = await fetch(`https://corona.lmao.ninja/all`);
     let data = await response.json()
     return data;
 }
 
-getUserAsync('yourUsernameHere')
-    .then(data => console.log(data)); 
+async function getAllCountries(country) {
+    let response = await fetch(`https://corona.lmao.ninja/countries/${country}`);
+    let data = await response.json()
+    return data;
+}
+
+async function getByTypes(type) {
+    let response = await fetch(`https://corona.lmao.ninja/countries?sort=${type}`);
+    let data = await response.json()
+    return data;
+}
+
+getAllCases()
+    .then(data => {
+        showAllCases(data.cases, allCases);
+        showAllCases(data.deaths, allDeaths);
+        showAllCases(data.recovered, allRecovered);
+    });
+
+getAllCountries('Nepal')
+    .then(data => {
+        showAllCases(data.cases, nepalCases);
+        showAllCases(data.deaths, nepalDeaths);
+        showAllCases(data.recovered, nepalRecovered);
+    });
+
+getAllCountries('Germany')
+    .then(data => {
+        showAllCases(data.cases, deCases);
+        showAllCases(data.deaths, deDeaths);
+        showAllCases(data.recovered, deRecovered);
+    });
+
+getByTypes('cases')
+    .then(data => {
+        console.log(data)
+        // showAllCases(data.cases, nepalCases);
+        // showAllCases(data.deaths, nepalDeaths);
+        // showAllCases(data.recovered, nepalRecovered);
+    });
+
+function showAllCases(data, element) {
+    element.innerText = data;
+}
