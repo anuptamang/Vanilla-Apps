6dc2eec61dc3622d5efcb52c3e89f411ba3253d2
Anup Tamang <anupkumartamang@gmail.com>
2020-03-17 22:21:19 +0545

    covid updated

--- 3.covid-19-tracker/src/js/app.js
+++ 3.covid-19-tracker/src/js/app.js
@@ -2,65 +2,49 @@
 import "isomorphic-fetch";
 import '../scss/main.scss';
 
-const allCases = document.querySelector('.all-cases');
-const allDeaths = document.querySelector('.all-deaths');
-const allRecovered = document.querySelector('.all-recovered');
+const allCases = selector('.all-cases');
+const allDeaths = selector('.all-deaths');
+const allRecovered = selector('.all-recovered');
 
-const nepalCases = document.querySelector('.nepal-cases');
-const nepalDeaths = document.querySelector('.nepal-deaths');
-const nepalRecovered = document.querySelector('.nepal-recovered');
+const nepalCases = selector('.nepal-cases');
+const nepalDeaths = selector('.nepal-deaths');
+const nepalRecovered = selector('.nepal-recovered');
 
-const deCases = document.querySelector('.de-cases');
-const deDeaths = document.querySelector('.de-deaths');
-const deRecovered = document.querySelector('.de-recovered');
+const deCases = selector('.de-cases');
+const deDeaths = selector('.de-deaths');
+const deRecovered = selector('.de-recovered');
 
-async function getAllCases() {
-    let response = await fetch(`https://corona.lmao.ninja/all`);
+async function getData(url) {
+    let response = await fetch(url);
     let data = await response.json()
     return data;
 }
 
-async function getAllCountries(country) {
-    let response = await fetch(`https://corona.lmao.ninja/countries/${country}`);
-    let data = await response.json()
-    return data;
-}
-
-async function getByTypes(type) {
-    let response = await fetch(`https://corona.lmao.ninja/countries?sort=${type}`);
-    let data = await response.json()
-    return data;
-}
-
-getAllCases()
+getData('https://corona.lmao.ninja/all')
     .then(data => {
         showAllCases(data.cases, allCases);
         showAllCases(data.deaths, allDeaths);
         showAllCases(data.recovered, allRecovered);
     });
 
-getAllCountries('Nepal')
+getData(`https://corona.lmao.ninja/countries/Nepal`)
     .then(data => {
         showAllCases(data.cases, nepalCases);
         showAllCases(data.deaths, nepalDeaths);
         showAllCases(data.recovered, nepalRecovered);
     });
 
-getAllCountries('Germany')
+getData(`https://corona.lmao.ninja/countries/Germany`)
     .then(data => {
         showAllCases(data.cases, deCases);
         showAllCases(data.deaths, deDeaths);
         showAllCases(data.recovered, deRecovered);
     });
 
-getByTypes('cases')
-    .then(data => {
-        console.log(data)
-        // showAllCases(data.cases, nepalCases);
-        // showAllCases(data.deaths, nepalDeaths);
-        // showAllCases(data.recovered, nepalRecovered);
-    });
-
 function showAllCases(data, element) {
     element.innerText = data;
+}
+
+function selector(name) {
+    return document.querySelector(name);
 }
