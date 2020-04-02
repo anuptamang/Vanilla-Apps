e0bee8edcf42f8ce397009fc9d13f1253a2bc54c
Anup Tamang <anupkumartamang@gmail.com>
2020-04-02 16:09:30 +0545

    digital timer added

---
+++ 4.digital-timer/timer.js
@@ -0,0 +1,50 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+
+    this.tick();
+    this.timerId = setInterval(this.tick, 20);
+  }
+
+  tick = () => {
+    if (parseFloat(this.timeRemaining) <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.02;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  }
+
+  pause = () => {
+    clearInterval(this.timerId);
+  }
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
