06b919062831bede700f389de1fb4eb93e20ab8d
Anup Tamang <anupkumartamang@gmail.com>
2020-04-07 13:18:30 +0545

    message sharing app added

--- 3.covid-19-tracker/src/js/app.js
+++ 3.covid-19-tracker/src/js/app.js
@@ -1,7 +1,5 @@
 //import dependency to work async/await/ promises/ fetch in IE
-import "isomorphic-fetch";
-// import './components/GoogleChartPlugin.js';
-// import './components/GoogleChart.js';
+import 'isomorphic-fetch';
 
 import '../scss/main.scss';
 
@@ -18,36 +16,33 @@ const uaDeaths = selector('.ua-deaths');
 const uaRecovered = selector('.ua-recovered');
 
 async function getData(url) {
-    let response = await fetch(url);
-    let data = await response.json()
-    return data;
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
 }
 
-getData('https://corona.lmao.ninja/all')
-    .then(data => {
-        showAllCases(data.cases, allCases);
-        showAllCases(data.deaths, allDeaths);
-        showAllCases(data.recovered, allRecovered);
-    });
+getData('https://corona.lmao.ninja/all').then((data) => {
+  showAllCases(data.cases, allCases);
+  showAllCases(data.deaths, allDeaths);
+  showAllCases(data.recovered, allRecovered);
+});
 
-getData(`https://corona.lmao.ninja/countries/Nepal`)
-    .then(data => {
-        showAllCases(data.cases, nepalCases);
-        showAllCases(data.deaths, nepalDeaths);
-        showAllCases(data.recovered, nepalRecovered);
-    });
+getData(`https://corona.lmao.ninja/countries/Nepal`).then((data) => {
+  showAllCases(data.cases, nepalCases);
+  showAllCases(data.deaths, nepalDeaths);
+  showAllCases(data.recovered, nepalRecovered);
+});
 
-getData(`https://corona.lmao.ninja/countries/Ukraine`)
-    .then(data => {
-        showAllCases(data.cases, uaCases);
-        showAllCases(data.deaths, uaDeaths);
-        showAllCases(data.recovered, uaRecovered);
-    });
+getData(`https://corona.lmao.ninja/countries/Ukraine`).then((data) => {
+  showAllCases(data.cases, uaCases);
+  showAllCases(data.deaths, uaDeaths);
+  showAllCases(data.recovered, uaRecovered);
+});
 
 function showAllCases(data, element) {
-    element.innerText = data;
+  element.innerText = data;
 }
 
 function selector(name) {
-    return document.querySelector(name);
+  return document.querySelector(name);
 }
