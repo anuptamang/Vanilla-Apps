44423c5818ee8314aa6af42a410be75400db8a01
Anup Tamang <anupkumartamang@gmail.com>
2020-04-20 22:35:03 +0545

    final cart updated

--- 8.e-com-app/routes/carts.js
+++ 8.e-com-app/routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -21,12 +23,48 @@ router.post('/cart/products', async (req, res) => {
   console.log(cart);
 
   // Either increment quanity for existing product or add new product
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
 
-  res.send('product added..');
+  if (existingItem) {
+    // increment quanity and save cart
+    existingItem.quantity++;
+  } else {
+    // add new poduct id to items array
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
+  res.redirect('/cart');
 });
 
 // Receive a GET request to show all items in cart
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 
 // Receive a post request to delete an item from a cart
+router.post('/cart/products/delete', async (req, res) => {
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+  await cartsRepo.update(req.session.cartId, { items });
+  res.redirect('/cart');
+});
 
 module.exports = router;
